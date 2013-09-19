fce6f04b6196dc0e642de0b2ed366e135d7457c3
crypt0s <bryanhalf@gmail.com>
2013-09-19 10:20:48 -0400

    Added stuff for anonymous login.

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+#Turning this on will attempt to connect to the server anonymously without using the domain credentials
+ANONYMOUS = True
+
 # Domain credentials for scanning
 USERNAME = ''
 PASSWORD = ''
@@ -6,7 +9,7 @@ DOMAIN = ""
 TARGET_LIST = "targets.txt"
 
 # This will output an "ls"-style output for easy grepping/awking
-OUTPUT_FILE = "output.txt"
+OUTPUT_FILE = "out.txt"
 
 # Limit the number of connections -- we make one SMB scanner thread per system in the targets file
 MAX_THREADS = 10
