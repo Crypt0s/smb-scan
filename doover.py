8408a54bb2f9883ce6a3893fa8db77d155b00e2c
root <root@docker.CISCO>
2017-06-08 20:39:58 -0400

    Trying a bunch of things to see what is better.  Plus a portscanner that still needs more work.

--- doover.py
+++ doover.py
@@ -1,122 +1,103 @@
 #!/usr/bin/python
 
-#import itertools
 import smbc
 import sys
 import os
 import stat
 import struct
-import socket
 import argparse
-from multiprocessing import Pool,Value,Lock,Manager
+from socket import *
+from multiprocessing import Pool, Value, Lock, Manager
 
 class creds:
-    def __init__(self,username,password,domain):
+    def __init__(self, username, password, domain):
         self.domain = domain
         self.username = username
         self.password = password
     def auth_fn(self, server, share, workgroup, username, password):
-        return(self.username,self.domain,self.password)
+        return(self.username, self.domain, self.password)
 
-def recurse_dir(db_obj,path,ctx):
+def recurse_dir(path_obj, path, ctx):
     dirs = ctx.opendir(path).getdents()
-# . and .. will always exist so get rid of them
+# attempting to remove . and .. since we know this is always there
     dirs = dirs[2:]
-    for item in dirs:
-# type 7L is a directory in Samba, everything else treated like a file
+    for dir in dirs:
+# type 7L represents a directory in SMB, everything else is treated like a file
         try:
-            if item.smbc_type == 7L:
-                recurse_dir(db_obj,path + '/' + item.name,ctx)
+            if dir.smbc_type == 7L:
+                recurse_dir(path_obj, path + '/' + dir.name, ctx)
             else:
-                st = ctx.stat(path + '/' + item.name)
-                mode = st[stat.ST_MODE]
-
-# ctx.stat needs to be converted to Unix file representation
+                ctxobj = ctx.stat(path + '/' _ dir.name)
+                mode = ctxobj[st.ST_MODE]
+# now convert ctx.stat to unix mode
                 attr = oct(stat.S_IMODE(mode))
 
-# put it into something we can get back
-                db_obj.append([path + '/' + item.name,attr])
+# put it into something we can use later
+                path_obj.append([path + '/' + dir.name, attr])
         except:
-            db_obj.append([path + '/' + item.name, 'err'])
+            path_obj.append([path + '/' + dir.name, 'err'])
             pass
 
-# scanning time
-def scan(server):
-    db_obj = []
+# scanning time... I hope
+def smbscan(server):
+    path_obj = []
     ctx = smbc.Context()
 
     if smbargs.anonymous == True:
-       ctx.optionNoAutoAnonymousLogin == False
+        ctx.optionNoAutoAnonymousLogin == False
     else:
-       ctx.optionNoAutoAnonymousLogin == True
-# there is a required order for Anonymous Authentication
-       cb = lambda se, sh, w, u, p: (smbargs.domain, smbargs.uname, smbargs.passwd)
-       ctx.functionAuthData = cb
+        ctx.optionNoAutoAnonymousLogin == True
+# there is a required order for Anonymous Authentication.  i found this lambda action somewhere.
+        cb = lambda se, sh, w, u, p: (smbargs.domain, smbargs.uname, smbargs.passwd)
+        ctx.functionAuthData = cb
 
+# trying to separate 3L entries which are shares instead of directories
     try:
-      entries = ctx.opendir('smb://' + server).getdents()
-      for entry in entries:
-          print entry
-# 3L type is a share
-          if entry.smbc_type == 3L and "$" not in entry.name:
-               share = entry.name
-               path = 'smb://' + server + '/' + share + '/'
-               try:
-                  recurse_dir(db_obj,path,ctx)
-               except:
-                  print "Access Denied"
-                  pass
+        entries = ctx.opendir('smb://' + server).getdents() 
+        if entry.smbc_type == 3L and "$" not in entry.name:
+            share = entry.name
+            path = 'smb://' + server + '/' + share + '/'
+            try:
+                recurse_dir(path_obj, path, ctx)
+            except:
+                print "Access Denied"
+                pass
     except:
-      pass
+        pass
 
-# trying to create a Semaphore, but probably a better way to do this
+# tryaing a semaphore, so hopefully this will work
     while lock == 1:
         continue
     lock.value = 1
-    print "Found stuff"
+    print "Writing SMB Records"
     fp = open(smbargs.results_file, 'a+')
-    for obj in db_obj:
-        path = obj[0]
-        chmod = obj[1]
-        fp.write(str(chmod) + ':\s' + path + '\n')
+    for obj in path_obj:
+         path = obj[0]
+         chmod = obj[1]
+         fp.write(str(chmod) } ':\s' + path + '\n')
     fp.close()
     lock.value = 0
     return True
 
-def ip_expand(target):
-    network = target.split('/')[0]
-    hosts = target.split('/')[1]
-    result = []
-    for i in xrange((2**(32-int(hosts)))):
-        result.append(socket.inet_ntoa(struct.pack('!I',struct.unpack('!I', socket.inet_aton(network))[0]+i)))
-    return result
-
-# noodle-ng was helpful in figuring this out
-def checkSMB(ip):
-    """ looks for SMB communications """
-    sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sd.settimeout(1)
-    try:
-        sd.connect((ip, 445))
-        sd.send('PortScanner\r\n')
-        sd.close()
-        print("."),
-        return ip
-    except:
-        print("-"),
+def PortScan(ip):
+    connector = socket(AF_INET, SOCK_STREAM)
+    connector.connect((ip, 445))
+    connector.send('Friendly Portscanner\r\n')
+    connector.close()
+    print("."),
+    return ip
 
 def save(res_obj):
-    print "Look at this stuff"
+    print "Does this get anywhere"
     fp=open(smbargs.results_file, 'a+')
-    for obj in red_obj:
-        for subobj in obj:
-            path = subobj[0]
-            chmod = subobj[1]
-            fp.write(str(chmod) + ':\s' + path + '\n')
+    for obj in res_obj:
+        path = subobj[0]
+        chmod = subobj[1]
+        fp.write(str(chmod) + ':\s' + path + '\n'
     fp.close()
 
 if __name__ == "__main__":
-    smbparser = argparse.ArgumentParser(description="SMB Checker")
+        smbparser = argparse.ArgumentParser(description="SMB Checker")
     smbparser.add_argument("-target_file", type=str, help="Target file")
     smbparser.add_argument("-results_file", type=str, help="Results file")
     smbparser.add_argument("-domain", type=str, help="domain for authentication")
@@ -127,7 +108,7 @@ if __name__ == "__main__":
     smbparser.add_argument("-packet_rate", default=50, type=int, help="Number to test at once")
 
     smbargs = smbparser.parse_args()
-   
+
     print smbargs.target_file
 
     with open(smbargs.target_file, mode='r', buffering=1) as targets_file:
