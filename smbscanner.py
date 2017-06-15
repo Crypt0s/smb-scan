88d1ab6cd64f77b83e6b2cfb0660dc195416d436
root <root@docker.CISCO>
2017-06-15 19:03:43 -0400

    First mergeable push of this fork.

--- smbscanner.py
+++ smbscanner.py
@@ -1,119 +1,68 @@
 #!/usr/bin/python
 
-#import itertools
 import smbc
 import sys
 import os
-import stat
-import struct
-import socket
 import argparse
-from multiprocessing import Pool,Value,Lock,Manager
+from socket import *
+from multiprocessing import Pool, Value, Lock, Manager
 
 class creds:
-    def __init__(self,username,password,domain):
+    def __init__(self, username, password, domain):
         self.domain = domain
         self.username = username
         self.password = password
     def auth_fn(self, server, share, workgroup, username, password):
-        return(self.username,self.domain,self.password)
+        return(self.username, self.domain, self.password)
 
-def recurse_dir(db_obj,path,ctx):
-    dirs = ctx.opendir(path).getdents()
-# . and .. will always exist so get rid of them
-    dirs = dirs[2:]
-    for item in dirs:
-# type 7L is a directory in Samba, everything else treated like a file
-        try:
-            if item.smbc_type == 7L:
-                recurse_dir(db_obj,path + '/' + item.name,ctx)
-            else:
-                st = ctx.stat(path + '/' + item.name)
-                mode = st[stat.ST_MODE]
-
-# ctx.stat needs to be converted to Unix file representation
-                attr = oct(stat.S_IMODE(mode))
-
-# put it into something we can get back
-                db_obj.append([path + '/' + item.name,attr])
-        except:
-            db_obj.append([path + '/' + item.name, 'err'])
-            pass
-
-# scanning time
-def scan(server):
-    db_obj = []
+# scanning time... I hope
+def smbscan(server):
+    path_obj = []
     ctx = smbc.Context()
 
     if smbargs.anonymous == True:
-       ctx.optionNoAutoAnonymousLogin == False
+        ctx.optionNoAutoAnonymousLogin == False
     else:
-       ctx.optionNoAutoAnonymousLogin == True
-# there is a required order for Anonymous Authentication
-       cb = lambda se, sh, w, u, p: (smbargs.domain, smbargs.uname, smbargs.passwd)
-       ctx.functionAuthData = cb
+        ctx.optionNoAutoAnonymousLogin == True
+# there is a required order for Anonymous Authentication.  i found this lambda action somewhere.
+        cb = lambda se, sh, w, u, p: (smbargs.domain, smbargs.uname, smbargs.passwd)
+        ctx.functionAuthData = cb
 
+# trying to separate 3L entries which are shares instead of directories
     try:
-      entries = ctx.opendir('smb://' + server).getdents()
-      for entry in entries:
-          print entry
-# 3L type is a share
-          if entry.smbc_type == 3L and "$" not in entry.name:
-               share = entry.name
-               path = 'smb://' + server + '/' + share + '/'
-               try:
-                  recurse_dir(db_obj,path,ctx)
-               except:
-                  print "Access Denied"
-                  pass
+        entries = ctx.opendir('smb://' + server).getdents()
+        for entry in entries:
+            print entry 
     except:
-      pass
+        pass
 
-# trying to create a Semaphore, but probably a better way to do this
+# trying a semaphore, so hopefully this will work
     while lock == 1:
         continue
     lock.value = 1
-    print "Found stuff"
+#    print "Writing SMB Records"
     fp = open(smbargs.results_file, 'a+')
-    for obj in db_obj:
-        path = obj[0]
-        chmod = obj[1]
-        fp.write(str(chmod) + ':\s' + path + '\n')
+    for obj in path_obj:
+         path = obj[0]
+         chmod = obj[1]
+         fp.write(str(chmod) + ':\s' + path + '\n')
     fp.close()
     lock.value = 0
     return True
 
-def ip_expand(target):
-    network = target.split('/')[0]
-    hosts = target.split('/')[1]
-    result = []
-    for i in xrange((2**(32-int(hosts)))):
-        result.append(socket.inet_ntoa(struct.pack('!I',struct.unpack('!I', socket.inet_aton(network))[0]+i)))
-    return result
-
-# noodle-ng was helpful in figuring this out
-def checkSMB(ip):
-    """ looks for SMB communications """
-    sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sd.settimeout(1)
+def PortScan(ip):
+    connector = socket(AF_INET, SOCK_STREAM)
+    connector.settimeout(1)
     try:
-        sd.connect((ip, 445))
-        sd.send('PortScanner\r\n')
-        sd.close()
-        print("."),
+        connector.connect((ip, 445))
+        connector.send('Friendly Portscanner\r\n')
+        smbbg = connector.recv(2048)
+        connector.close()
+        print("+ " + smbbg + '\n')
         return ip
     except:
-        print("-"),
-
-def save(res_obj):
-    print "Look at this stuff"
-    fp=open(smbargs.results_file, 'a+')
-    for obj in red_obj:
-        for subobj in obj:
-            path = subobj[0]
-            chmod = subobj[1]
-            fp.write(str(chmod) + ':\s' + path + '\n')
-    fp.close()
+        print("- " + ip + "- Port 445 Closed")
+        return ip
 
 if __name__ == "__main__":
     smbparser = argparse.ArgumentParser(description="SMB Checker")
@@ -124,11 +73,11 @@ if __name__ == "__main__":
     smbparser.add_argument("-passwd", type=str, help="Password for authentication")
     smbparser.add_argument("-anonymous", default=False, type=str, help="Use True to test for Anonymous access.")
 #    smbparser.add_argument("--ip_range", type=str, help="CIDR block, use /32 for individuals")
-    smbparser.add_argument("-packet_rate", default=50, type=int, help="Number to test at once")
+    smbparser.add_argument("-packet_rate", default=1, type=int, help="Number to test at once")
 
     smbargs = smbparser.parse_args()
-   
-    print smbargs.target_file
+
+#    print smbargs.target_file
 
     with open(smbargs.target_file, mode='r', buffering=1) as targets_file:
         targets = targets_file.readlines()
@@ -143,23 +92,19 @@ if __name__ == "__main__":
                  expand_range = expand_range + ip_expand(targets[i])
                  targets.pop(i)
                  targets = targets + expand_range
-                 print "Checking for SMB communication protocols"
-# remove target from the target list that aren't running the SMB protocol
-    pool = Pool(smbargs.packet_rate)
-    valid_targets = pool.map(checkSMB, targets)
+                 print "Checking for communication"
 
-# This hopefully will edit a list in-place
+    pool = Pool(smbargs.packet_rate)
+    valid_targets = pool.map(PortScan, targets)
+
     valid_targets[:] = (x for x in valid_targets if x is not None)
     print valid_targets
-    print str(len(valid_targets))+" Valid targets found."
-    del targets
-
     print "Starting to crawl the targets... this will take some time."
     lock = Value('i',0,lock=True)
 
 # dynamic scanning pool, so we'll take care of this here
 
     npool = Pool(smbargs.packet_rate)
-    results = npool.map_async(scan,valid_targets)
+    results = npool.map_async(smbscan, valid_targets)
     results.get()
     print "Done"
